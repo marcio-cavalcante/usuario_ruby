cf6599cb4a2b3e663d5886d6f8901916eb32251f
marcio-cavalcante <marcio_acc@hotmail.com>
2025-01-27 18:47:58 -0300

    Add files via upload

---
+++ usuario.rb
@@ -0,0 +1,8 @@
+puts "Digite seu nome:"
+nome = gets.chomp
+puts "Digite seu sobrenome:"
+sobrenome = gets.chomp
+puts "Digite sua idade:"
+idade = gets.chomp.to_i
+
+puts "Olá #{nome} #{sobrenome}, bem vindo. Você tem #{idade} anos."
